d2604bf4f0193980842fc4c5f8ccbaedad9576eb
zarapeisker <zarapeisker@gmail.com>
2017-06-29 11:31:51 +0700

    completed basic layout for prototype home

---
+++ src/app/shared/testimonial-tile.component/testimonial-tile.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-testimonial-tile',
+  templateUrl: 'testimonial-tile.component.html',
+  styleUrls: ['testimonial-tile.component.scss']
+})
+
+export class ReviewsTileComponent { }
--- src/app/shared/tile_helper.module.ts
+++ src/app/shared/tile_helper.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
 import {FeatureTileComponent} from './feature-tile.component/feature-tile.component';
 import {GetStartedTileComponent} from './get-started.component/get-started-tile.component';
-import {ReviewsTileComponent} from './reviews-tile.component/reviews-tile.component';
+import {ReviewsTileComponent} from './testimonial-tile.component/testimonial-tile.component';
 import {SearchTileComponent} from './search-tile.component/search-tile.component';
 import {TitleTileComponent} from './title-tile.component/title-tile.component';
 
